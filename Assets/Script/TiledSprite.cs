b5c023b7104d97c74d417afed672f8cad8e0d32c
dill <dill_yu@163.com>
2015-03-19 14:02:37 +0800

    ddd

---
+++ Assets/Script/TiledSprite.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TiledSprite : MonoBehaviour {
+
+	private tk2dTiledSprite tSprite;
+	// Use this for initialization
+	void Start () 
+	{
+
+		tSprite = gameObject.GetComponent<tk2dTiledSprite>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnMouseEnter()
+	{
+
+		tSprite.color = Color.yellow;
+		//test
+		//ok
+		
+	}
+	void OnMouseExit()
+	{
+		tSprite.color = Color.white;
+		
+	}
+}
