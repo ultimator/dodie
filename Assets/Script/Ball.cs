f019058d4d45c72b10b85a610b4b75a791880070
dill <dill_yu@163.com>
2015-04-06 00:56:47 +0800

    fgh

--- Assets/Script/Ball.cs
+++ Assets/Script/Ball.cs
@@ -4,12 +4,37 @@ using System.Collections;
 public class Ball : MonoBehaviour {
 
 	// Use this for initialization
-	void Start () {
+	public TextMesh overText;
+	void Start () 
+	{
 	
 	}
-	
+
+	void OnTriggerEnter(Collider other)
+	{
+		//if(other.tag == "pointer" && _state == RockStateEnum.act1)
+		//{
+		//    _state = RockStateEnum.act2;
+		//}
+		if(other.tag == "weapon" )
+		{
+			overText.text = "Game Over";
+			overText.fontSize = 64;
+			Instantiate(overText, new Vector3(1,2,0), Quaternion.identity);
+			GamePause ();
+		}
+		
+		
+	}
+	void GamePause()
+	{
+		Time.timeScale = 0;
+	}
 	// Update is called once per frame
-	void Update () {
+
+	void Update () 
+	{
+		//GamePause ();
 	
 	}
 }
