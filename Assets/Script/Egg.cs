5b2aecbe09eba091114d5e1fbb6dee79adeb5d87
dill <dill_yu@163.com>
2015-04-21 11:14:18 +0800

    asdf

---
+++ Assets/Script/Egg.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Egg : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	void OnTriggerEnter(Collider other)
+	{
+		//if(other.tag == "pointer" && _state == RockStateEnum.act1)
+		//{
+		//    _state = RockStateEnum.act2;
+		//}
+		if(other.tag == "weapon")
+		{
+			//Destroy(gameObject);
+		}
+		
+		
+	}
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
